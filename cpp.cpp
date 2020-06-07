ec19632dba35c4d419e1f6bd59bf8fc4c316d2cb
nevikw39 <nevik.w39@gmail.com>
2020-06-07 22:58:15 +0800

    Add cpp.cpp

---
+++ cpp.cpp
@@ -0,0 +1,10 @@
+#include <bits/extc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+using namespace __gnu_pbds;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
