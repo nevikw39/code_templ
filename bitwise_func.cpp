17bdd01a0125ba80687cc57632e2fb19d14071ef
nevikw39 <nevik.w39@gmail.com>
2021-02-08 16:01:27 +0800

    Rename bitwise functions

--- bitwise_func.cpp
+++ bitwise_func.cpp
@@ -1,15 +1,15 @@
 template <typename T = int64_t>
-T inline myabs(T x)
+T inline bwabs(T x)
 {
     return (x ^ (x >> ((1 << sizeof(T)) - 1))) - (x >> ((1 << sizeof(T)) - 1));
 }
 template <typename T = int64_t>
-T inline mymin(T x, T y)
+T inline bwmin(T x, T y)
 {
     return y ^ ((x ^ y) & -(x < y));
 }
 template <typename T = int64_t>
-T inline mymax(T x, T y)
+T inline bwmax(T x, T y)
 {
     return x ^ ((x ^ y) & -(x < y));
 }
