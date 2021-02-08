1cbdbd377d5dd8ae1f9d7852126493c3d9934bff
nevikw39 <nevik.w39@gmail.com>
2021-02-08 15:59:37 +0800

    Update disjoint set.

--- disjoint_set.cpp
+++ disjoint_set.cpp
@@ -1,34 +1,36 @@
 struct disjoint_set
 {
-    int n;
+    int n, g;
     vector<int> p, r;
     disjoint_set(int _n) : n(_n + 1)
     {
-        r = vector<int>(n);
+        g = n;
+        r = vector<int>(n, 1);
         for (int i = 0; i < n;)
             p.push_back(i++);
     }
     int find(int x)
     {
-        if (x >= n)
-            return -1;
-        if (p[x] == x)
-            return x;
-        return p[x] = find(p[x]);
+        return p[x] == x ? x : p[x] = find(p[x]);
     }
-    void unite(int x, int y)
+    bool unite(int x, int y)
     {
         x = find(x);
         y = find(y);
         if (x == y)
-            return;
+            return false;
+        --g;
         if (r[x] < r[y])
+        {
             p[x] = y;
+            r[y] += r[x];
+        }
         else
         {
             p[y] = x;
-            r[x] += r[x] == r[y];
+            r[x] += r[y];
         }
+        return true;
     }
     bool same(int x, int y)
     {
