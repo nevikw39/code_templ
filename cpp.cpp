d1db04d916aa024c6fef1c2af40d08660fb8d776
nevikw39 <nevik.w39@gmail.com>
2021-02-08 16:00:34 +0800

    Update cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,10 +1,35 @@
+/**                  _ _              _____ ___  
+ *  _ __   _____   _(_) | ____      _|___ // _ \ 
+ * | '_ \ / _ \ \ / / | |/ /\ \ /\ / / |_ \ (_) |
+ * | | | |  __/\ V /| |   <  \ V  V / ___) \__, |
+ * |_| |_|\___| \_/ |_|_|\_\  \_/\_/ |____/  /_/ 
+ **/
 #include <bits/extc++.h>
-#pragma GCC optimize("Ofast")
+#ifndef nevikw39
+#define nevikw39      \
+    cin.tie(nullptr); \
+    ios::sync_with_stdio(false);
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#pragma GCC target("abm,bmi,bmi2,mmx,sse,sse2,sse3,ssse3,sse4,popcnt,avx,avx2,fma,tune=native")
+#pragma comment(linker, "/stack:200000000")
+struct _cerr
+{
+    template <typename T>
+    _cerr &operator<<(const T &x) { return *this; }
+} __cerr;
+#define cerr __cerr
+#else
+#pragma message("hello, nevikw39")
+#endif
+#define ALL(X) begin(X), end(X)
+#define ST first
+#define ND second
+#pragma message("GL; HF!")
 using namespace std;
+using namespace __gnu_cxx;
 using namespace __gnu_pbds;
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    nevikw39;
     return 0;
 }
