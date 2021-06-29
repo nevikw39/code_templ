6229354268d40758ab1a9882fc2f331761613112
牜攵 <nevik.w39@gmail.com>
2021-06-29 21:13:37 +0800

    Update math_func.cpp

--- math_func.cpp
+++ math_func.cpp
@@ -10,7 +10,7 @@ T mulmod(T a, T b, T m)
 template <typename T = int64_t>
 T binpow(T a, T n, T m)
 {
-    T y = 0;
+    T y = 1;
     for (a %= m; n; n >>= 1, a = mulmod(a, a, m))
         if (n & 1)
             y = mulmod(y, a, m);
