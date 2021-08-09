be52fda9594504c41cdbd39df1d2eb36b57203b6
牜攵 <nevik.w39@gmail.com>
2021-08-09 18:51:58 +0800

    Update bitwise_func.cpp

--- bitwise_func.cpp
+++ bitwise_func.cpp
@@ -1,7 +1,7 @@
 template <typename T = int64_t>
 T inline bwabs(T x)
 {
-    return (x ^ (x >> ((sizeof(int64_t) << 3) - 1))) - (x >> ((sizeof(int64_t) << 3) - 1));
+    return (x ^ (x >> ((sizeof(T) << 3) - 1))) - (x >> ((sizeof(T) << 3) - 1));
 }
 template <typename T = int64_t>
 T inline bwmin(T x, T y)
