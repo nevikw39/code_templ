ef4b714221af73fb149aa76a56c23585847a5299
牜攵 <nevik.w39@gmail.com>
2022-03-18 15:12:43 +0800

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,20 @@
+void dijkstra(int n, const vector<vector<pair<int, int>>> &g, int s, vector<int64_t> &d)
+{
+    _heap<pair<int64_t, int>> pq;
+    vector<_heap<pair<int64_t, int>>::point_iterator> itrs(n);
+    itrs[s] = pq.push({d[s] = 0, s});
+    while (pq.size())
+    {
+        int t = pq.top().ND;
+        pq.pop();
+        itrs[t] = nullptr;
+        for (const auto &[i, w] : g[t])
+            if (d[i] > d[t] + w)
+            {
+                if (itrs[i] != nullptr)
+                    pq.modify(itrs[i], {d[i] = d[t] + w, i});
+                else
+                    itrs[i] = pq.push({d[i] = d[t] + w, i});
+            }
+    }
+}
